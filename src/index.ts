5e3a4c7dc29514d2a6266be855bffc85ab4db49d
mouarius <mouarius@gmail.com>
2021-11-04 20:02:57 +0100

    Basic connection and deconnection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import { Player } from "./config/types";
 
-const app = express();
 const PORT = 4000;
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "http://localhost:8080", methods: ["GET", "POST"] },
+});
 
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
-app.listen(PORT, () => {
+let players: Player[] = [];
+
+function initializePlayer(socket: Socket) {
+  const newPlayer: Player = {
+    id: socket.id,
+    color: "",
+  };
+  players.push(newPlayer);
+}
+
+function removePlayer(socket: Socket) {
+  players = players.filter((p) => p.id !== socket.id);
+}
+
+app.use("/", (_req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user has connected.");
+  // TODO : Ask a username for the player
+
+  initializePlayer(socket);
+
+  console.log(players);
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected.");
+    removePlayer(socket);
+  });
+});
+
+httpServer.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
+// A Game has started, there is already a list of players connected
