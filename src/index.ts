3f7f2ebc646f5a09945d78cfbe2edd43d8e9d09e
mouarius <mouarius@gmail.com>
2021-11-14 18:51:11 +0100

    Implementing login functionnality 1.0

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import { Player } from "./config/types";
+import cors from "cors";
 
 const PORT = 4000;
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: { origin: "http://localhost:8080", methods: ["GET", "POST"] },
+  cors: { origin: "*", methods: ["GET", "POST"] },
 });
 
+const users = {};
 let players: Player[] = [];
+let rooms = [];
 
 function initializePlayer(socket: Socket) {
   const newPlayer: Player = {
@@ -24,8 +27,19 @@ function removePlayer(socket: Socket) {
   players = players.filter((p) => p.id !== socket.id);
 }
 
-app.use("/", (_req, res) => {
+app.use(cors());
+
+app.get("/", (_req, res) => {
   res.send("<h1>Hello world</h1>");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  // if (!Object.keys(users).includes(body.username)) {
+  //   res.status(400).send("Invalid username");
+  // }
+  console.log("A user wanted to login with username : ");
+  res.status(200).send("Logged in successfully");
 });
 
 io.on("connection", (socket) => {
@@ -33,8 +47,14 @@ io.on("connection", (socket) => {
   // TODO : Ask a username for the player
 
   initializePlayer(socket);
-
   console.log(players);
+
+  socket.on("hello", () => {
+    console.log('The user said "Hello"');
+  });
+  socket.on("GET/PLAYER_LIST", () => {
+    io.emit("GET/PLAYER_LIST/RESPONSE", JSON.stringify(players));
+  });
 
   socket.on("disconnect", () => {
     console.log("A user has disconnected.");
